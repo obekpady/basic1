72e3eef0efaf45cbec5b38f50a8599daea99d9b7
obekpady <mdobekpayusuf@gmail.com>
2022-09-26 15:13:59 +0100

    edit, delete and update on Multi Image

--- app/Http/Controllers/Home/AboutController.php
+++ app/Http/Controllers/Home/AboutController.php
@@ -94,7 +94,7 @@ class AboutController extends Controller
                 'alert-type' => 'success'
             );
         
-            return redirect()->back()->with($notification);
+            return redirect()->route('all.multi.image')->with($notification);
       
 
 
@@ -105,5 +105,53 @@ class AboutController extends Controller
         $allMultiImage = MultiImage::all();
 
         return view('admin.about_page.all_multiimage', compact('allMultiImage'));
-    }
+    }//end of AllMultiImage
+
+    //Edit Multi Image
+    public function EditMultiImage($id){
+        $multiImage = MultiImage::findOrFail($id);
+
+        return view('admin.about_page.edit_multi_image', compact('multiImage'));
+
+    }//end of edit multiImage
+
+
+    //update image method
+    public function UpdateMultiImage(Request $request){
+        $multi_image_id = $request->id;
+        if($request->file('multi_image')){
+            $image =$request->file('multi_image');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension(); //3434363534.jpg
+            Image::make($image)->resize(220,220)->save('upload/multi/'.$name_gen);
+            $save_url = 'upload/multi/'.$name_gen;
+            
+            
+            
+            MultiImage::findOrFail($multi_image_id)->update([
+                'multi_image'=> $save_url,
+            ]);
+            $notification = array(
+                'message' => 'Image updated successfully',
+                'alert-type' => 'success'
+            );
+        
+            return redirect()->route('all.multi.image')->with($notification);
+        }
+
+    }//end method
+
+    //Delete image method
+public function DeleteMultiImage($id){
+    $multi = MultiImage::findOrFail($id);
+    $img = $multi->multi_image;
+    unlink($img);
+    MultiImage::findOrFail($id)->delete();
+    $notification = array(
+    'message' => 'Image deleted successfully',
+    'alert-type' => 'success'
+);
+
+return redirect()->back()->with($notification);
+
+    }//end delete method
 }
